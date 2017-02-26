c45b0cf527ca024e891615fcddde482c35bdb4cc
Matthew J. Geiger <matthew.j.geiger@gmail.com>
2017-02-26 15:42:12 -0500

    Changed to timer_twisted

--- timer_twisted.py
+++ timer_twisted.py
@@ -1,23 +1,110 @@
 from twisted.internet import task
 from twisted.internet import reactor
-from datetime import datetime
+from datetime import datetime, date, timedelta
+import logging
+from astral import Location
+import pytz
+import os
+from subprocess import call
 
-camera_timeout = 1.0
-system_timeout = (24.0 * 60.0 * 60.0)
 
-prior_camera = datetime.now()
+# Folder Locations
+base_dir = '/opt/camera'
+img_dir = os.path.join(base_dir, 'img')
+vid_dir = os.path.join(base_dir, 'vid')
+
+# Interval time for trying the camera
+camera_timeout = 60.0
+
+# Location information
+latitude = 42.288611
+longitude = -71.445
+
+logging.basicConfig(format='%(asctime)s : %(message)s', 
+        datefmt='%Y-%m-%d %H:%M:%S', 
+        level=logging.INFO)
+
+
+class TimeLapseCamera():
+    def __init__(self):
+        pass
+
+    def take_picture(self):
+        pass
+
+    def store_picture(self):
+        pass
+
+    def close(self):
+        pass
+    
+
+
+def sun_up():
+    """
+    Determines if the sun is up right now.
+    """
+    loc = Location(('Framingham, MA', 'USA', latitude, longitude, 'US/Eastern', 0))
+    sunrise = loc.sunrise()
+    sunset = loc.sunset()
+    now = datetime.now(pytz.timezone('US/Eastern'))
+    return sunset > now > sunrise
 
 def capture_camera():
-    print('{}: camera'.format(datetime.now()))
-    print('{}'.format(datetime.now() - prior_camera))
-    prior_camera = datetime.now()
+    """
+    This will store the file name into today's directory.
+    """
+    logging.info('Capturing camera. Insert Raspistill here.')
+    prog = '/usr/bin/raspistill'
+    today = date.today()
+    img_today_dir = os.path.join(img_dir, 
+            str(today.year), 
+            str(today.month).zfill(2), 
+            str(today.day).zfill(2))
+    # Check if today's directory exists
+    if not os.path.exists(img_today_dir):
+        os.makedirs(img_today_dir)
+    output = '{}.jpg'.format()
+    full_output = os.path.join(img_today_dir, output)
+    call_list = [prog, '--height', '720', '--width', '1280', '--output', full_output]
+    call(call_list)
 
-def do_video():
-    print('{}: video'.format(datetime.now()))
+def is_one_am():
+    """
+    Determines if we are between 01:00 and 01:01
+    """
+    today = date.today()
+    today_one_am = datetime(year=today.year, month=today.month, 
+            day=today.day, hour=1)
+    above_one = datetime.now() >= today_one_am
+    less_one = (today_one_am + timedelta(days=1)) > datetime.now()
+    return above_one and less_one 
 
-loop_camera = task.LoopingCall(do_camera)
+def make_video():
+    logging.info('Generating video.')
+    # Figure out yesterday's video path
+    yesterday = date.today() - timedelta(days=1)
+    img_yesterday_dir = os.path.join(img_dir, 
+            str(yesterday.year), 
+            str(yesterday.month).zfill(2), 
+            str(yesterday.day).zfill(2))
+
+def upload_youtube():
+    logging.error('Not implemented.')
+    logging.info('Uploading to Youtube.')
+
+def timelapse_arbiter():
+    logging.info('Running timelapse arbiter')
+    if sun_up():
+        logging.info('Sun is up. Capturing image.')
+        capture_camera()
+    if is_one_am():
+        logging.info('Making video and uploading.')
+        make_video()
+        upload_youtube()
+
+logging.info('Starting logging timer.')
+loop_camera = task.LoopingCall(timelapse_arbiter)
 loop_camera.start(camera_timeout)
-loop_video = task.LoopingCall(do_video)
-loop_video.start(system_timeout)
 
 reactor.run()
